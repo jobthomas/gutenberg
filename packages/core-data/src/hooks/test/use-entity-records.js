af018437d398712cc2389d5bd55a45147d5d1c25
George Mamadashvili <georgemamadashvili@gmail.com>
2026-03-17 15:06:19 +0400

    Feedback

--- packages/core-data/src/hooks/test/use-entity-records.js
+++ packages/core-data/src/hooks/test/use-entity-records.js
@@ -53,7 +53,7 @@ describe( 'useEntityRecords', () => {
 			hasResolved: false,
 			hasStarted: false,
 			isResolving: false,
-			status: 'IDLE',
+			status: 'idle',
 			totalItems: null,
 			totalPages: null,
 		} );
@@ -70,7 +70,7 @@ describe( 'useEntityRecords', () => {
 			hasResolved: true,
 			hasStarted: true,
 			isResolving: false,
-			status: 'SUCCESS',
+			status: 'finished',
 			totalItems: 3,
 			totalPages: 1,
 		} );
