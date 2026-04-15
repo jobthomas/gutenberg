72955b045e43cf5aabc4734b8c3bb0c967fb22d4
Andrew Duthie <andrew.duthie@automattic.com>
2026-04-15 10:31:45 -0400

    Comment non-working tsconfigPath

--- storybook/main.ts
+++ storybook/main.ts
@@ -61,7 +61,7 @@ const config: StorybookConfig = {
 		// Should match defaults in Storybook except for the propFilter.
 		// https://github.com/storybookjs/storybook/blob/3e34a288c8fabc7d5b5cc43b28ae9d674c48e3ea/code/core/src/core-server/presets/common-preset.ts#L162-L168
 		reactDocgenTypescriptOptions: {
-			tsconfigPath: './tsconfig.json',
+			// tsconfigPath: './tsconfig.json',
 			EXPERIMENTAL_useProjectService: true,
 			shouldExtractLiteralValuesFromEnum: true,
 			shouldRemoveUndefinedFromOptional: true,
